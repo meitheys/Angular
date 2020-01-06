acc61fb0238749e5dc32e47978921e7fb4ad2b82
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-01-06 10:33:11 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CdkDragDrop, transferArrayItem, moveItemInArray } from '@angular/cdk/drag-drop';
+
+export interface Character {
+  name: string;
+  image: string;
+  fatality: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'drag-drop';
+
+  myList: Character[];
+  confirmList: Character[] = [];
+
+  constructor(private httpCliente: HttpClient){
+    this.getMyList()
+  }
+
+  getMyList(){
+    this.httpCliente.get<Character[]>("assets/data.json")
+    .subscribe(list =>{
+      this.myList = list;
+    })
+  }
+
+  drop(event: CdkDragDrop<Character[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+
+
+}
+
 }
